68483314d1ffe0cd5f99c284cc8565a12f1306fc
adamko7 <addiiaa1234@gmail.com>
2024-06-20 18:41:02 +0200

    Menu update

--- projekt1.cpp
+++ projekt1.cpp
@@ -23,8 +23,13 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+        cout << "1. Oblicz siline" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+
+        if (wyjscie == 1) {
+            cout << factorial(a) << endl;
+        }
     } while(wyjscie != 0);
     return 0;
 }
